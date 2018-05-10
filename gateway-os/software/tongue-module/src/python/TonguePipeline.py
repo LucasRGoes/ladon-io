1a7d5b91955f8875b3e065dfa5fec27c4ad42abc
Lucas Góes <lucas.rd.goes@gmail.com>
2018-05-10 01:52:15 -0300

    Modificações no tongue-module para enviar os arquivos disponíveis apenas uma vez

--- gateway-os/software/tongue-module/src/python/TonguePipeline.py
+++ gateway-os/software/tongue-module/src/python/TonguePipeline.py
@@ -2,11 +2,11 @@
 import argparse							# Argparse: the recommended command-line parsing module
 import json 							# Json: is a lightweight data interchange format inspired by JavaScript object literal syntax
 import logging							# Logging: provides a set of convenience functions for simple logging usage
+import os 								# OS: this module provides a portable way of using operating system dependent functionality 
 import random							# Random: this module implements pseudo-random number generators for various distributions
+import time								# Time: provides various time-related functions
 
 from kafka import KafkaProducer			# KafkaProducer: is a high-level, asynchronous message producer
-
-## ladon05121995
 
 ## FUNCTIONS ##
 
@@ -24,6 +24,7 @@ def argumentParserFactory():
 	parser.add_argument("-k", "--kafka", type=str, default="ladonio.ddns.net", help="the kafka broker")
 	parser.add_argument("--mqtt-topic", type=str, default="#", help="the mqtt topic to subscribe")
 	parser.add_argument("--kafka-topic", type=str, default="ladon", help="the kafka topic to publish")
+	parser.add_argument("-f", "--frequency", type=int, default=30, help="the frequency to send data")
 
 	return parser
 
@@ -45,29 +46,47 @@ args = argumentParserFactory().parse_args()
 
 # Configuring logging module
 logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(name)s]: %(message)s', level=args.verbosity * 10)
+logger = logging.getLogger("TonguePipeline")
 
-## TESTING ##
+# Enter loop
+while True:
+	# Lists files inside captures folder
+	listFiles = os.listdir("captures")
+	
+	# Verify if there is something inside the folder
+	if len(listFiles) > 0:
+		toSendFileName = listFiles[0]
+		logger.info("Sending: {}".format(toSendFileName))
 
-# Opens image
-photoFile = open("/home/lucas/Downloads/Teste.png", "rb")
-fileContent = photoFile.read()
-photoFile.close()
+		# Reads file
+		toSendFile = open("captures/{}".format(toSendFileName), "rb")
+		toSend = toSendFile.read()
+		toSendFile.close()		
 
-# Creates a producer
-producer = KafkaProducer(
-			bootstrap_servers=args.kafka,
-			value_serializer=lambda v: json.dumps(v, default=serializeBytes).encode('utf-8'),
-			retries=5
-		   )
+		# Creates a kafka producer
+		producer = KafkaProducer(
+					bootstrap_servers=args.kafka,
+					value_serializer=lambda v: json.dumps(v, default=serializeBytes).encode('utf-8'),
+					retries=5
+				   )
 
-# Creates a hash for this photo
-photoHash = random.getrandbits(128)
+		# For each chunk of the image
+		try:
+			for chunkId, chunk in enumerate(chunks(toSend, 10240)):
+				# Sends message
+				future = producer.send('ladon', value={'file_name': toSendFileName, 'chunk_id': chunkId, 'chunk': chunk})
+				result = future.get(timeout=60)
+				producer.flush()
+				logger.info("Sent Chunk ID: {}".format(chunkId))
 
-# For each chunk of the image
-for chunkId, chunk in enumerate(chunks(fileContent, 10240)):
-	# Sends message
-	future = producer.send('ladon', value={'hash': photoHash, 'chunk_id': chunkId, 'chunk': chunk})
-	result = future.get(timeout=60)
-	producer.flush()
+			logger.info("File succesfully sent")
+			os.rename("captures/{}".format(toSendFileName), "sent/{}".format(toSendFileName))
 
-	print("Sent Chunk ID: {}".format(chunkId))
+		except:
+			logger.error("Failed to send")
+
+	else:
+		logger.info("Nothing to send")
+
+	# Sleeps for the chosen number of minutes
+	time.sleep(args.frequency * 60)

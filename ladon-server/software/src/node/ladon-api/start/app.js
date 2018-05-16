7fe10bb78f513e38a460929bf41f1993d7b7bb93
Lucas Góes <lucas.rd.goes@gmail.com>
2018-05-15 23:25:06 -0300

    Adding adonis-4-mongoose and its configs to project

--- ladon-server/software/src/node/ladon-api/start/app.js
+++ ladon-server/software/src/node/ladon-api/start/app.js
@@ -15,7 +15,8 @@ const providers = [
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
+  '@adonisjs/lucid/providers/LucidProvider',
+  'adonis-4-mongoose/provider/Mongoose'
 ]
 
 /*
@@ -43,7 +44,9 @@ const aceProviders = [
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {}
+const aliases = {
+	Mongoose: 'Adonis/Addons/AdonisMongoose'
+}
 
 /*
 |--------------------------------------------------------------------------
